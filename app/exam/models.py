12e7c0da5f696a3b27cc109ff02c3a42dd7bc8b8
Joao Assalim <joassalim@gmail.com>
2025-12-19 19:26:39 -0300

    chore: create Exam Submissions Models

--- app/exam/models.py
+++ app/exam/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 from question.models import Question
+from student.models import Student
+from question.utils import AlternativesChoices
 
 
 class Exam(models.Model):
@@ -22,3 +24,31 @@ class ExamQuestion(models.Model):
 
     def __str__(self):
         return f'{self.question} - {self.exam}'
+
+class ExamSubmission(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE, related_name="submissions")
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="exam_submissions")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.student.email} - {self.exam.name} ({self.created_at:%Y-%m-%d %H:%M:%S})"
+
+
+class ExamSubmissionAnswer(models.Model):
+    submission = models.ForeignKey(
+        ExamSubmission,
+        on_delete=models.CASCADE,
+        related_name="answers",
+    )
+    exam_question = models.ForeignKey(ExamQuestion, on_delete=models.CASCADE, related_name="submission_answers")
+    answer = models.IntegerField(choices=AlternativesChoices)
+
+    class Meta:
+        unique_together = ("submission", "exam_question")
+        indexes = [
+            models.Index(fields=["submission", "exam_question"]),
+            models.Index(fields=["exam_question"]),
+        ]
